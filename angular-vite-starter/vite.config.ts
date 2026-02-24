91bf6236983ec7b5698808e49bda30c3adae70fd
pracht <prachtdev@gmail.com>
2026-02-24 22:24:58 +0100

    Initialize Angular Vite project with configuration files and dependencies

---
+++ angular-vite-starter/vite.config.ts
@@ -0,0 +1,14 @@
+import {defineConfig} from 'vite';
+import angular from '@analogjs/vite-plugin-angular';
+
+
+export default defineConfig({
+
+  resolve: {
+    mainFields: ['module'],
+  },
+  plugins: [angular()],
+  server: {
+    port: 4200,
+  }
+})
